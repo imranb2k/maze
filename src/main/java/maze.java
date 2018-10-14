0a0ea8fda0256cc228c2a32a1285e4648a6663cf
imran <imranb2k@hotmail.com>
2018-10-14 20:02:46 +0100

    create read maze from file method and test

--- src/main/java/maze.java
+++ src/main/java/maze.java
@@ -1,6 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class maze {
 
-    private void imput (){
+    public List read (String inputFile){
+
+        ArrayList maze = new ArrayList();
+
+        try{
+
+            Reader reader = new FileReader(inputFile);
+
+            BufferedReader br =
+                    new BufferedReader(reader);
+
+
+            while(br.ready()) {
+
+                maze.add(br.readLine());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return maze;
 
     }
 }
---
+++ src/test/java/MazeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class MazeTest {
+
+
+    @Test
+    public void testReadMaze() {
+
+
+    }
+
+}
